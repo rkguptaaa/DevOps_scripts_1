c7664b98443e5552927fe38c9dc5425a94f5276b
R. K. Gupta <rkguptaaa@gmail.com>
2024-05-22 05:29:51 +0530

    2nd commit

--- script.sh
+++ script.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
 
-country=India
+country=Japan
 
 echo $country
+
+# how to take input from user?
+# read cmd
+
+echo "Hello, please enter yopur name here"
+read name
+echo "Hey, $name welcome.."
+
+# Conditions
+# If Else
+
+age=16;
+if [ $age = 18 ];
+then
+    echo "You are eligible to vote"
+elif [ $age -gt 18 ];
+then
+    echo "you are eligible to vote as older then 18"
+else
+    echo "not eligible, thanks"
+fi
+
+#ctrl K+U -- Uncomment
+#ctrl K+C -- Comment
